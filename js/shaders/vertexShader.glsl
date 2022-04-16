55924fb75d7f88f4a0d89f5a5fd281211575165d
Ata Ergir <66826811+iloveonions@users.noreply.github.com>
2022-04-16 13:26:58 +0200

    Update vertexShader.glsl

--- js/shaders/vertexShader.glsl
+++ js/shaders/vertexShader.glsl
@@ -7,7 +7,6 @@ float PI = 3.141529;
 
 vec3 deformationCurve(vec3 pos, vec2 uv, vec2 offset) {
     pos.y += 0.5 * sin(uv.y * PI) * offset.y;
-    // pos.x += 5. * sin(uv.y * PI) * offset.x;
     return pos;
 }
 
